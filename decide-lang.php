f199783ef55b037fb22b67d096fcf5223527ae67
Anis-Ichigo <82107862+Anis-Ichigo@users.noreply.github.com>
2021-06-08 11:19:25 +0200

    Update decide-lang.php

--- decide-lang.php
+++ decide-lang.php
@@ -1,6 +1,6 @@
 <?php
 
-
+if (isset($_POST["enregistrer_parametres"])){
 if ($_POST['lang']=='fr') {           // si la langue est 'fr' (français) on inclut le fichier fr-lang.php
 include('fr-lang.php');
 }
@@ -8,7 +8,7 @@ include('fr-lang.php');
 else if ($_POST['lang']=='en') {      // si la langue est 'en' (anglais) on inclut le fichier en-lang.php
  include('en-lang.php');
 }
-
+}
 else {                       // si aucune langue n'est déclarée on inclut le fichier fr-lang.php par défaut
 include('fr-lang.php');
 }								
