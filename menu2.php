2af3c3230e2f68e5d20073d9a8492a974bcc4e91
MarineCbl <cabirol.marine@orange.fr>
2021-06-10 11:36:43 +0200

    Update menu2.php

--- menu2.php
+++ menu2.php
@@ -22,32 +22,32 @@ require('decide-lang.php');
     <div class="menu">
       <a href="reservation_portable.php">
         <div class="menu__item" id="0-0"> <img class="menu__icon" src="signe-plus-dans-un-cercle-noir.svg" />
-          <div class="menu__content"><span class="menu__span"></span><?php echo TXT_ACCUEIL_NOUVELLER; ?><span class="menu__span"><?php echo TXT_ACCUEIL_NOUVELLER; ?></span></div>
+          <div class="menu__content"><span class="menu__span"></span><span class="menu__span"></span></div>
       </a>
     </div>
     <a href="mes_reservations.php">
       <div class="menu__item" id="1-0"><img class="menu__icon" src="livre.svg" />
-        <div class="menu__content"><span class="menu__span"><?php echo TXT_ACCUEIL_RESERVATION; ?></span><span class="menu__span"><?php echo TXT_ACCUEIL_RESERVATION; ?></span></div>
+        <div class="menu__content"><span class="menu__span"></span><span class="menu__span"></span></div>
       </div>
     </a>
     <a href="profil.php">
       <div class="menu__item" id="0-1"><img class="menu__icon" src="forme-de-l'utilisateur.svg" />
-        <div class="menu__content"><span class="menu__span"><?php echo TXT_ACCUEIL_PROFIL; ?></span><span class="menu__span"><?php echo TXT_ACCUEIL_PROFIL; ?></span></div>
+        <div class="menu__content"><span class="menu__span"></span><span class="menu__span"></span></div>
       </div>
     </a>
     <a href="FAQ.php">
       <div class="menu__item" id="1-1"><img class="menu__icon" src="signe-de-question.svg" />
-        <div class="menu__content"><span class="menu__span"><?php echo FAQ; ?></span><span class="menu__span"><?php echo FAQ; ?></span></div>
+        <div class="menu__content"><span class="menu__span"></span><span class="menu__span"></span></div>
       </div>
     </a>
     <a href="reglage.php">
       <div class="menu__item" id="0-2"><img class="menu__icon" src="silhouette-de-roue-dentee.svg" />
-        <div class="menu__content"><span class="menu__span"><?php echo TXT_ACCUEIL_REGLAGE; ?></span><span class="menu__span"><?php echo TXT_ACCUEIL_REGLAGE; ?></span></div>
+        <div class="menu__content"><span class="menu__span"></span><span class="menu__span"></span></div>
       </div>
     </a>
     <a href="deconnexion.php">
       <div class="menu__item" id="1-2"><img class="menu__icon" src="option-de-deconnexion.svg" />
-        <div class="menu__content"><span class="menu__span"><?php echo DECONNEXION; ?></span><span class="menu__span"><?php echo DECONNEXION; ?></span></div>
+        <div class="menu__content"><span class="menu__span"></span><span class="menu__span"></span></div>
       </div>
     </a>
 
