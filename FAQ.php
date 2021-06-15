735b6db3a9b7d74e681c6da5cf45dd8e51880881
haoyang314 <haoyang0314@gmail.com>
2021-06-15 19:07:22 +0200

    FAQ CSS

--- FAQ.php
+++ FAQ.php
@@ -14,6 +14,8 @@ require('decide-lang.php');
     <link href="Style_FAQ.css" type="text/css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
 </head>
 
 <body>
@@ -21,7 +23,7 @@ require('decide-lang.php');
         <img alt="Logo UT1" class="img_logo" src="Bandeau.png" style="width: 100%; margin-top: 1%;">
     </div>
     <div class="contenu">
-        <h1> <?php echo TXT_ACCUEIL_FAQ; ?> </h1>
+        <h2 class="text-center"> <?php echo TXT_ACCUEIL_FAQ; ?> </h2>
         <section class="faq">
             <strong><?php echo TXT_QUESTION1; ?></strong>
             <p align="justify"> <?php echo TXT_REPONSE1_A; ?> <a href="reservation_portable.php"><i>"<?php echo TXT_REPONSE1_B; ?>"</i></a><?php echo TXT_REPONSE1_C; ?>
@@ -61,12 +63,15 @@ require('decide-lang.php');
 
 
     <div class="text-center">
-        <input onclick="history.go(-1)" type="button" class="btn btn-secondary" value="<?php echo TXT_MENU; ?>" style="text-transform: uppercase; font-size: 300%;">
+        <a href="menu3.php" type="button" class="btn btn-secondary mb-3" style="text-transform: uppercase;"><?php echo TXT_MENU; ?></a>
+
     </div>
 
+    <!--
     <footer style="text-align: center; font-size: 1.5em; bottom:0; position:absolute; width:100%; background-color: #ffc0cb;">
         Site réalisé par Marine CABIROL - Haoyang YU - Lisa DE SMET - Anis MANA - Antoine LAVIGNE
     </footer>
+    -->
 </body>
 
 
