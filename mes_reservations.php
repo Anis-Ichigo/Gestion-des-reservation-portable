60007130912728a357125eeb417d4d99cd6cb4e5
MarineCbl <cabirol.marine@orange.fr>
2021-06-15 10:28:33 +0200

    Update mes_reservations.php

--- mes_reservations.php
+++ mes_reservations.php
@@ -220,6 +220,44 @@ date_default_timezone_set('Europe/Paris');
         <?php
 
         if (isset($_POST['confirmer_prolongation'])) {
+          $jour_p = $_POST['DateProlongation'];
+        if(strftime('%A', strtotime($jour_p)) == "Saturday" || strftime('%A', strtotime($jour_p)) == "Sunday" ){
+          ?>
+          <div class="modal fade" id="alerte" tabindex="-1" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                      <div class="modal-body">
+                          <div class="alert alert-danger d-flex align-items-center" role="alert">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                            </svg>
+
+                              <div>
+                                  <?php echo TXT_ERREUR_DATE; ?>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="modal-footer">
+                          <div class="col text-center">
+                              <input type="button" class="btn btn-primary" data-bs-dismiss="modal" value="<?php echo TXT_OK; ?>">
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <?php
+          echo "<script>
+              $(window).load(function() {
+                $('#alerte').modal('show');
+                });
+                </script>";
+        }else{
+
+
+
+
             $identifiantE = $_POST['IdentifiantE'];
             $identifiantM = $_POST['IdentifiantM'];
             $dateProlongation = $_POST['DateProlongation'];
@@ -296,6 +334,7 @@ date_default_timezone_set('Europe/Paris');
     </script>";
             }
         }
+    }
         ?>
 
         <?php
